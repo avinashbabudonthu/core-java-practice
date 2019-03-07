c6ce297f741565f1cc264c893e4f897032da5ced
Free Lance Projects <freelancepjts@gmail.com>
2019-03-07 20:48:08 +0530

    collections, file io

---
+++ collections/src/main/java/com/collections/App.java
@@ -0,0 +1,13 @@
+package com.collections;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ file-io/src/main/java/com/file/io/App.java
@@ -0,0 +1,13 @@
+package com.file.io;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
