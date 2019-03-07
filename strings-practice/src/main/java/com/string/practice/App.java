a413109d1218ac7d093ff611645db5b3f83271cd
Free Lance Projects <freelancepjts@gmail.com>
2019-03-07 07:44:15 +0530

    core java practice initial check in

---
+++ strings-practice/src/main/java/com/string/practice/App.java
@@ -0,0 +1,13 @@
+package com.string.practice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
