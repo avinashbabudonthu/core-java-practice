19a5110bca26361a8b87507b31b712eed0dbf69e
Free Lance Projects <freelancepjts@gmail.com>
2019-03-07 07:48:21 +0530

    date api practice

---
+++ date-api-practice/src/main/java/com/date/api/practice/App.java
@@ -0,0 +1,13 @@
+package com.date.api.practice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
