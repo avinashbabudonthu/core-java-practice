5392b508899803c5b749b1614abf196c6b9aaaf0
Donthu, Avinash(CORP) <Avinash.Donthu@ADP.com>
2019-04-04 06:26:54 +0530

    string format

--- strings-practice/src/test/java/com/string/practice/StringFormatPractice.java
+++ strings-practice/src/test/java/com/string/practice/StringFormatPractice.java
@@ -1,50 +1,47 @@
 package com.string.practice;
 
-import java.lang.invoke.MethodHandles;
-
 import org.junit.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class StringFormatPractice {
-
-	private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
 	@Test
 	public void stringFormat() {
 		String str1 = String.format("Welcome to number %d, %d, %d and %d", 1, 2, 3, 4);
-		LOGGER.info("str1={}", str1);
+		log.info("str1={}", str1);
 
 		String str2 = String.format("Decimal value with 2 decimals: %.2f", 1.2356);
-		LOGGER.info("str2={}", str2);
+		log.info("str2={}", str2);
 
 		String str3 = String.format("%d, %o, %x, %X", 32, 32, 32, 32);
-		LOGGER.info("str3={}", str3);
+		log.info("str3={}", str3);
 
 		String str4 = String.format("%d, %#o, %#x, %#X", 32, 32, 32, 32);
-		LOGGER.info("str4={}", str4);
+		log.info("str4={}", str4);
 
-		LOGGER.info("-- right justify -- ");
+		log.info("-- right justify -- ");
 		String str5 = String.format("num1:%4d, num2:%4d ", 6, 287);
 		String str6 = String.format("num1:%4d, num2:%4d ", 657, 7);
 		String str7 = String.format("num1:%04d, num2:%04d ", 877, 6);
-		LOGGER.info("str5={}", str5);
-		LOGGER.info("str6={}", str6);
-		LOGGER.info("str7={}", str7);
+		log.info("str5={}", str5);
+		log.info("str6={}", str6);
+		log.info("str7={}", str7);
 
-		LOGGER.info("-- left justify -- ");
+		log.info("-- left justify -- ");
 		String str8 = String.format("num1:%-4d, num2:%-4d ", 6, 287);
 		String str9 = String.format("num1:%-4d, num2:%-4d ", 657, 7);
-		LOGGER.info("str8={}", str8);
-		LOGGER.info("str9={}", str9);
+		log.info("str8={}", str8);
+		log.info("str9={}", str9);
 
-		LOGGER.info("-- group separator");
+		log.info("-- group separator");
 		String str10 = String.format("%d", 1234567);
 		String str11 = String.format("%,d", 1234567);
 		String str12 = String.format("%,.2f", 123457.6);
-		LOGGER.info("str10={}", str10);
-		LOGGER.info("str11={}", str11);
-		LOGGER.info("str12={}", str12);
+		log.info("str10={}", str10);
+		log.info("str11={}", str11);
+		log.info("str12={}", str12);
 
 		String s13 = String.format("%d", 123);
 		String s14 = String.format("%d", -123);
@@ -55,14 +52,14 @@ public class StringFormatPractice {
 		String s19 = String.format("%(d", 123);
 		String s20 = String.format("%(d", -123);
 		String s21 = String.format("% (d", 123);
-		LOGGER.info("s13={}", s13);
-		LOGGER.info("s14={}", s14);
-		LOGGER.info("s15={}", s15);
-		LOGGER.info("s16={}", s16);
-		LOGGER.info("s17={}", s17);
-		LOGGER.info("s18={}", s18);
-		LOGGER.info("s19={}", s19);
-		LOGGER.info("s20={}", s20);
-		LOGGER.info("s21={}", s21);
+		log.info("s13={}", s13);
+		log.info("s14={}", s14);
+		log.info("s15={}", s15);
+		log.info("s16={}", s16);
+		log.info("s17={}", s17);
+		log.info("s18={}", s18);
+		log.info("s19={}", s19);
+		log.info("s20={}", s20);
+		log.info("s21={}", s21);
 	}
 }
