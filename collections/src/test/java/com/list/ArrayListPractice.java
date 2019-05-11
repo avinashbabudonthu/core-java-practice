2954c8d2ee9b370919e77af8cc0ae8df3b7bc0bd
Donthu, Avinash(CORP) <Avinash.Donthu@ADP.com>
2019-05-11 17:20:37 +0530

    list examples

--- collections/src/test/java/com/list/ArrayListPractice.java
+++ collections/src/test/java/com/list/ArrayListPractice.java
@@ -1,5 +1,139 @@
 package com.list;
 
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+@Slf4j
 public class ArrayListPractice {
 
+    @Test
+    public void add(){
+        // method 1
+        List<String> list1 = new ArrayList<>();
+        list1.add("1");
+        list1.add("2");
+        list1.add("3");
+        log.info("list1={}", list1);
+
+        // method 2
+        List<String> list2 = Arrays.asList("4","5","6");
+        log.info("list={}", list2);
+    }
+
+    @Test
+    public void addAll(){
+        List<String> list1 = new ArrayList<>();
+        list1.add("1");
+        list1.add("2");
+        log.info("before list1={}", list1);
+
+        List<String> list2 = new ArrayList<>();
+        list2.add("3");
+        list2.add("4");
+        log.info("list2={}", list2);
+
+        list1.addAll(list2);
+        log.info("after list1={}", list1);
+    }
+
+    @Test
+    public void remove(){
+        List<String> list1 = new ArrayList<>();
+        list1.add("1");
+        list1.add("2");
+        list1.add("3");
+        list1.add("4");
+        log.info("original - list1={}", list1);
+
+        list1.remove("1");
+        log.info("remove element 1 - list1={}", list1);
+
+        list1.remove(2);
+        log.info("remove element by index - list1={}", list1);
+    }
+
+    @Test
+    public void removeAll(){
+        List<String> list1 = new ArrayList<>();
+        list1.add("a");
+        list1.add("b");
+        list1.add("c");
+        list1.add("d");
+        log.info("list1 before={}", list1);
+
+        List<String> list2 = new ArrayList<>();
+        list2.add("c");
+        log.info("list2={}", list2);
+
+        list1.removeAll(list2);
+        log.info("list1 after={}", list1);
+    }
+
+    @Test
+    public void subList(){
+        List<String> list1 = new ArrayList<>();
+        list1.add("1");
+        list1.add("2");
+        list1.add("3");
+        list1.add("4");
+        list1.add("5");
+        log.info("list1={}", list1);
+
+        List<String> subList = list1.subList(1, 3);
+        log.info("sublist={}", subList);
+
+        // adding element to subList will reflect in actual list
+        subList.add("6");
+        log.info("subList={}", subList);
+        log.info("list1={}", list1);
+
+        // deleting element from subList will reflect in actual list
+        subList.remove("3");
+        log.info("subList={}", subList);
+        log.info("list1={}", list1);
+    }
+
+    @Test
+    public void iterate(){
+        List<String> list1 = new ArrayList<>();
+        list1.add("1");
+        list1.add("2");
+        list1.add("3");
+        list1.add("4");
+        list1.add("5");
+
+        // using for loop
+        for(int i=0;i<list1.size();i++){
+            log.info("list1[{}]={}", i, list1.get(i));
+        }
+
+        // for each
+        for(String element : list1){
+            log.info("element={}", element);
+        }
+
+        // stream
+        list1.stream().forEach(element -> log.info("element={}", element));
+    }
+
+    @Test
+    public void toArray() {
+        List<String> list1 = new ArrayList<>();
+        list1.add("1");
+        list1.add("2");
+        list1.add("3");
+
+        // to object array
+        Object[] objectArray = list1.toArray();
+        Stream.of(objectArray).forEach(element -> log.info("element={}", element));
+
+        // to String array
+        String[] stringArray = list1.toArray(new String[list1.size()]);
+        Stream.of(stringArray).forEach(element -> log.info("element={}", element));
+    }
 }
--- collections/src/test/java/com/list/LinkedListPractice.java
+++ collections/src/test/java/com/list/LinkedListPractice.java
@@ -1,4 +1,20 @@
 package com.list;
 
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+import java.util.LinkedList;
+import java.util.List;
+
+@Slf4j
 public class LinkedListPractice {
+
+    @Test
+    public void add(){
+        List<String> list1 = new LinkedList<>();
+        list1.add("1");
+        list1.add("2");
+        list1.add("3");
+        log.info("list1={}", list1);
+    }
 }
