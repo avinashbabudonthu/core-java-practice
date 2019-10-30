728c1331a899a19232ea8898cef0f22af418ecdd
Donthu, Avinash(CORP) <Avinash.Donthu@ADP.com>
2019-10-30 15:37:39 +0530

    updated readme

--- strings-practice/src/test/java/com/string/practice/StringFormatPractice.java
+++ strings-practice/src/test/java/com/string/practice/StringFormatPractice.java
@@ -10,38 +10,38 @@ public class StringFormatPractice {
 	@Test
 	public void stringFormat() {
 		String str1 = String.format("Welcome to number %d, %d, %d and %d", 1, 2, 3, 4);
-		log.info("str1={}", str1);
+		log.info("str1={}", str1); // str1=Welcome to number 1, 2, 3 and 4
 
 		String str2 = String.format("Decimal value with 2 decimals: %.2f", 1.2356);
-		log.info("str2={}", str2);
+		log.info("str2={}", str2); // str2=Decimal value with 2 decimals: 1.24
 
 		String str3 = String.format("%d, %o, %x, %X", 32, 32, 32, 32);
-		log.info("str3={}", str3);
+		log.info("str3={}", str3); // str3=32, 40, 20, 20
 
 		String str4 = String.format("%d, %#o, %#x, %#X", 32, 32, 32, 32);
-		log.info("str4={}", str4);
+		log.info("str4={}", str4); // str4=32, 040, 0x20, 0X20
 
 		log.info("-- right justify -- ");
 		String str5 = String.format("num1:%4d, num2:%4d ", 6, 287);
 		String str6 = String.format("num1:%4d, num2:%4d ", 657, 7);
 		String str7 = String.format("num1:%04d, num2:%04d ", 877, 6);
-		log.info("str5={}", str5);
-		log.info("str6={}", str6);
-		log.info("str7={}", str7);
+		log.info("str5={}", str5); // str5=num1:   6, num2: 287
+		log.info("str6={}", str6); // str6=num1: 657, num2:   7
+		log.info("str7={}", str7); // str7=num1:0877, num2:0006
 
 		log.info("-- left justify -- ");
 		String str8 = String.format("num1:%-4d, num2:%-4d ", 6, 287);
 		String str9 = String.format("num1:%-4d, num2:%-4d ", 657, 7);
-		log.info("str8={}", str8);
-		log.info("str9={}", str9);
+		log.info("str8={}", str8); // str8=num1:6   , num2:287
+		log.info("str9={}", str9); // str9=num1:657 , num2:7
 
 		log.info("-- group separator");
 		String str10 = String.format("%d", 1234567);
 		String str11 = String.format("%,d", 1234567);
 		String str12 = String.format("%,.2f", 123457.6);
-		log.info("str10={}", str10);
-		log.info("str11={}", str11);
-		log.info("str12={}", str12);
+		log.info("str10={}", str10); // str10=1234567
+		log.info("str11={}", str11); // str11=1,234,567
+		log.info("str12={}", str12); // str12=123,457.60
 
 		String s13 = String.format("%d", 123);
 		String s14 = String.format("%d", -123);
@@ -52,14 +52,14 @@ public class StringFormatPractice {
 		String s19 = String.format("%(d", 123);
 		String s20 = String.format("%(d", -123);
 		String s21 = String.format("% (d", 123);
-		log.info("s13={}", s13);
-		log.info("s14={}", s14);
-		log.info("s15={}", s15);
-		log.info("s16={}", s16);
-		log.info("s17={}", s17);
-		log.info("s18={}", s18);
-		log.info("s19={}", s19);
-		log.info("s20={}", s20);
-		log.info("s21={}", s21);
+		log.info("s13={}", s13); // s13=123
+		log.info("s14={}", s14); // s14=-123
+		log.info("s15={}", s15); // s15= 123
+		log.info("s16={}", s16); // s16=-123
+		log.info("s17={}", s17); // s17=+123
+		log.info("s18={}", s18); // s18=-123
+		log.info("s19={}", s19); // s19=123
+		log.info("s20={}", s20); // s20=(123)
+		log.info("s21={}", s21); // s21= 123
 	}
 }
--- strings-practice/src/test/java/com/string/practice/StringJoinerPractice.java
+++ strings-practice/src/test/java/com/string/practice/StringJoinerPractice.java
@@ -13,29 +13,29 @@ public class StringJoinerPractice {
 	public void commaSeparatedString() {
 		StringJoiner str = new StringJoiner(",");
 		str.add("jack").add("jim").add("jill");
-		log.info("str={}", str);
+		log.info("str={}", str); // str=jack,jim,jill
 
 		StringJoiner str2 = new StringJoiner(",", "{", "}");
 		str2.add("jack").add("jim").add("jill");
-		log.info("str2={}", str2);
+		log.info("str2={}", str2); // str2={jack,jim,jill}
 
 		StringJoiner str3 = new StringJoiner("],[", "[", "]");
 		str3.add("jack").add("jim").add("jill");
-		log.info("str3={}", str3);
+		log.info("str3={}", str3); // str3=[jack],[jim],[jill]
 
 		StringJoiner str4 = new StringJoiner(",", "{", "}");
 		str4.add("jack");
-		log.info("str4={}", str4);
+		log.info("str4={}", str4); // str4={jack}
 
 		StringJoiner str5 = new StringJoiner(",", "{", "}");
-		log.info("str5={}", str5);
+		log.info("str5={}", str5); // str5={}
 
 		StringJoiner str6 = new StringJoiner(",");
 		str6.setEmptyValue("EMPTY");
-		log.info("str6={}", str6);
+		log.info("str6={}", str6); // str6=EMPTY
 
 		StringJoiner str7 = new StringJoiner(",", "{", "}");
 		str7.setEmptyValue("EMPTY");
-		log.info("str7={}", str7);
+		log.info("str7={}", str7); // str7=EMPTY
 	}
 }
