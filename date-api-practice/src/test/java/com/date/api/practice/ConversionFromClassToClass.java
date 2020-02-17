0bcb28c46e999d2ea76c591e8b29378e74504b3b
Donthu, Avinash(CORP) <Avinash.Donthu@ADP.com>
2020-02-17 18:01:24 +0530

    core java practice

--- date-api-practice/src/test/java/com/date/api/practice/ConversionFromClassToClass.java
+++ date-api-practice/src/test/java/com/date/api/practice/ConversionFromClassToClass.java
@@ -2,6 +2,7 @@ package com.date.api.practice;
 
 import java.time.LocalDate;
 import java.time.ZoneId;
+import java.util.Date;
 import java.util.GregorianCalendar;
 
 import javax.xml.datatype.DatatypeFactory;
@@ -18,8 +19,7 @@ public class ConversionFromClassToClass {
 	@Test
 	public void localDateToGregorianCalendar() {
 		LocalDate localDate = LocalDate.now();
-		GregorianCalendar gregorianCalendar = GregorianCalendar
-				.from(localDate.atStartOfDay(ZoneId.systemDefault()));
+		GregorianCalendar gregorianCalendar = GregorianCalendar.from(localDate.atStartOfDay(ZoneId.systemDefault()));
 		log.info("date={}", gregorianCalendar.toString());
 	}
 
@@ -27,10 +27,43 @@ public class ConversionFromClassToClass {
 	@Test
 	public void localDateToXmlGregorianCalendar() {
 		LocalDate localDate = LocalDate.now();
-		GregorianCalendar gregorianCalendar = GregorianCalendar
-				.from(localDate.atStartOfDay(ZoneId.systemDefault()));
+		GregorianCalendar gregorianCalendar = GregorianCalendar.from(localDate.atStartOfDay(ZoneId.systemDefault()));
 		XMLGregorianCalendar xmlGregorianCalendar = DatatypeFactory.newInstance()
 				.newXMLGregorianCalendar(gregorianCalendar);
 		log.info("date={}", xmlGregorianCalendar.toString());
+	}
+
+	@Test
+	public void utilDateToXmlGregorianCalendar() {
+		Date date = new Date();
+		GregorianCalendar gregorianCalendar = new GregorianCalendar();
+		gregorianCalendar.setTime(date);
+		XMLGregorianCalendar xmlGregorianCalendar = null;
+		try {
+			xmlGregorianCalendar = DatatypeFactory.newInstance().newXMLGregorianCalendar(gregorianCalendar);
+		} catch (Exception e) {
+			log.error("Exception while creating XMLGregorianCalendar", e);
+		}
+
+		log.info("XMLGregorianCalendar={}", xmlGregorianCalendar);
+	}
+
+	@Test
+	public void xmlGregorianCalendarToUtilDate() {
+		// create XMLGregorianCalendar
+		Date date = new Date();
+		GregorianCalendar gregorianCalendar = new GregorianCalendar();
+		gregorianCalendar.setTime(date);
+		XMLGregorianCalendar xmlGregorianCalendar = null;
+		try {
+			xmlGregorianCalendar = DatatypeFactory.newInstance().newXMLGregorianCalendar(gregorianCalendar);
+		} catch (Exception e) {
+			log.error("Exception while creating XMLGregorianCalendar", e);
+		}
+
+		// convert to util date
+		Date resultUtilDate = xmlGregorianCalendar.toGregorianCalendar().getTime();
+
+		log.info("result util-date={}", resultUtilDate);
 	}
 }
