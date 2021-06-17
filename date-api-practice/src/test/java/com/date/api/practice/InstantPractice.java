fef73f9831dcd157b40b1ec56d56e27808300f71
Avinash Babu Donthu <avinashbabu.donthu@gmail.com>
2021-06-17 08:41:26 +0530

    instant examples

--- date-api-practice/src/test/java/com/date/api/practice/InstantPractice.java
+++ date-api-practice/src/test/java/com/date/api/practice/InstantPractice.java
@@ -3,6 +3,7 @@ package com.date.api.practice;
 import java.time.Instant;
 import java.time.OffsetDateTime;
 import java.time.ZoneOffset;
+import java.util.Date;
 
 import org.junit.Test;
 
@@ -12,9 +13,25 @@ import lombok.extern.slf4j.Slf4j;
 public class InstantPractice {
 
 	@Test
+	public void createInstant() {
+		Instant instant = Instant.now();
+		log.info("instant={}", instant); //instant=2021-06-17T03:10:30.169Z
+	}
+
+	@Test
 	public void instantToOffsetDateTime() {
 		Instant instant = Instant.now();
-		OffsetDateTime offsetDateTime2 = instant.atOffset(ZoneOffset.ofHours(7));
-		log.info("offsetDateTime2={}", offsetDateTime2);
+		OffsetDateTime offsetDateTime = instant.atOffset(ZoneOffset.ofHours(7));
+		log.info("offsetDateTime={}", offsetDateTime); // offsetDateTime=2021-06-17T10:10:44.697+07:00
 	}
+
+	@Test
+	public void instantToUtilDate() {
+		Instant instant = Instant.now();
+		log.info("instant={}", instant); // instant=2021-06-17T03:10:01.710Z
+
+		Date date = Date.from(instant);
+		log.info("date={}", date); // date=Thu Jun 17 08:40:01 IST 2021
+	}
+
 }
