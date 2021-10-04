ba368dc80c77913d3357ca69fd608a0c096fcf6d
Avinash Babu Donthu <avinashbabu.donthu@gmail.com>
2021-10-04 23:30:53 +0530

    file ip

---
+++ file-io/src/test/java/com/file/io/RandomAccessFilePractice.java
@@ -0,0 +1,14 @@
+package com.file.io;
+
+public class RandomAccessFilePractice {
+
+    @org.junit.Test
+    public void readFromFile(){
+
+    }
+
+    @org.junit.Test
+    public void writeToFile(){
+
+    }
+}
