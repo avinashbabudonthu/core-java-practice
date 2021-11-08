5f0c0c3b8ea48a5f16ce8a931fd516475420d7aa
Avinash Babu Donthu <avinashbabu.donthu@gmail.com>
2021-11-08 21:59:32 +0530

    random access file

--- file-io/src/test/java/com/file/io/RandomAccessFilePractice.java
+++ file-io/src/test/java/com/file/io/RandomAccessFilePractice.java
@@ -1,14 +1,27 @@
 package com.file.io;
 
+import java.io.RandomAccessFile;
+
 public class RandomAccessFilePractice {
 
+    @lombok.SneakyThrows
     @org.junit.Test
     public void readFromFile(){
-
+        RandomAccessFile randomAccessFile = new RandomAccessFile("src/test/resources/file1.txt", "r");
+        randomAccessFile.seek(0);
+        byte[] bytes = new byte[11];
+        randomAccessFile.read(bytes);
+        randomAccessFile.close();
+        String output = new String(bytes);
+        System.out.println(output);
     }
 
+    @lombok.SneakyThrows
     @org.junit.Test
     public void writeToFile(){
-
+        RandomAccessFile randomAccessFile = new RandomAccessFile("src/test/resources/file1.txt", "rw");
+        randomAccessFile.seek(0);
+        randomAccessFile.write("Hello World".getBytes());
+        randomAccessFile.close();
     }
 }
